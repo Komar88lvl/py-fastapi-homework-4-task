52164d7b7998822b9f493427a81f37d2b6193cf1
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-08-11 20:28:37 +0300

    Create ProfileSchema

--- src/schemas/profiles.py
+++ src/schemas/profiles.py
@@ -10,4 +10,32 @@ from validation import (
     validate_birth_date
 )
 
-# Write your code here
+class ProfileSchema(BaseModel):
+    first_name: str = Form(...),
+    last_name: str = Form(...),
+    gender: str = Form(...),
+    date_of_birth: date = Form(...),
+    info: str = Form(...),
+    avatar: UploadFile = File(...)
+
+    @field_validator("first_name")
+    def validate_name(cls, value):
+        return validate_name(value)
+
+    @field_validator("last_name")
+    def validate_name(cls, value):
+        return validate_name(value)
+
+    @field_validator("gender")
+    def validate_gender(cls, value):
+        return validate_gender(value)
+
+    @field_validator("date_of_birth")
+    def validate_birth_date(cls, value):
+        return validate_birth_date(value)
+
+    @field_validator("info")
+    def validate_info(cls, value):
+        if not value.strip():
+            raise ValueError("Info cannot be empty or spaces only.")
+        return value
